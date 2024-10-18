d5d514dc0356e8d815b3fb44ce4fdc443ed52764
Lydia Vonhuben <lydia.vonhuben@students.gymneufeld.ch>
2024-10-18 09:57:02 +0200

    Kommentare geschrieben

--- minimax.py
+++ minimax.py
@@ -18,15 +18,16 @@ spielfeld = [
 ]
 
 def evaluation(spielfeld):
+    """Schaut ob das Spiel beendet ist und ob Gewonnen oder Unendschieden"""
     for voraussetzungen in gewinn_voraussetzung:
         a = spielfeld[voraussetzungen[0][0]][voraussetzungen[0][1]]
         b = spielfeld[voraussetzungen[1][0]][voraussetzungen[1][1]]
         c = spielfeld[voraussetzungen[2][0]][voraussetzungen[2][1]]
         if a == b and b == c:
             if a == 1:
-                return 1  # KI gewinnt
+                return 1  # Computer gewinnt
             elif a == -1:
-                return -1  # Mensch gewinnt
+                return -1  # Spieler gewinnt
     
     if all(item != 0 for row in spielfeld for item in row):
         return 0  # Unentschieden
@@ -42,28 +43,29 @@ def spiel_beendet(spielfeld):
     return False
 
 def minimax(spielfeld, tiefe, max_spieler):
+    """Rekursiver Aufruf des Minimax Algorithmus um Spielbaum zu bewerten."""
     ergebnis = evaluation(spielfeld)
     if ergebnis is not None:
         if ergebnis == 1:
             return 10 - tiefe  # Positive Bewertung je früher der Gewinn
         elif ergebnis == -1:
-            return tiefe - 10  # Negative Bewertung je früher der Verlust
+            return tiefe - 10  # Negative Bewertung je früher der Gewinn
         else:
             return 0  # Unentschieden
     
-    if max_spieler:  # KI
+    if max_spieler:  # Computer
         beste_bewertung = -1000
         for child in kinder_von_spielfeld(spielfeld, max_spieler):
             beste_bewertung = max(beste_bewertung, minimax(child, tiefe + 1, False))
         return beste_bewertung
-    else:  # Mensch
+    else:  # Spieler
         beste_bewertung = 1000
         for child in kinder_von_spielfeld(spielfeld, max_spieler):
             beste_bewertung = min(beste_bewertung, minimax(child, tiefe + 1, True))
         return beste_bewertung
 
-
 def kinder_von_spielfeld(spielfeld, max_spieler):
+    """Erstellt den Spielbaum"""
     spielfelder_liste = []
     
     for i in range(3):
--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 from random import randrange
 from minimax import minimax, evaluation
+
 # -1:X, 0: leer, 1:O
 spielfeld = [
     [0, 0, 0],
@@ -18,12 +19,13 @@ gewinn_voraussetzung = [
     [(0,2), (1,1), (2,0)]
 ]
 
-
 def spielfeld_anzeigen():
+    """Druckt Spielfeld in das Terminal"""
     for row in spielfeld:
         print(row)
 
 def eingabe_pruefen(eingegebener_zug):
+    """Prüft ob die Eingabe des Spielers korrekt ist"""
     global spielfeld
     try:
         y_kord, x_kord = int(eingegebener_zug[0])-1, int(eingegebener_zug[1])-1
@@ -31,7 +33,7 @@ def eingabe_pruefen(eingegebener_zug):
             if spielfeld[y_kord][x_kord] == 0:
                 return True, (x_kord, y_kord)
             else:
-                print("Ungültiger Spielzug: Feld bereits belegt") #nume bi -1 mache u nid bi pc
+                print("Ungültiger Spielzug: Feld bereits belegt")
                 return False, None
         else:
             print("Ungültiger Spielzug: Koordinaten außerhalb des Spielfelds")
@@ -42,6 +44,7 @@ def eingabe_pruefen(eingegebener_zug):
 
 def eingabe():
     '''
+    Koordinaten des Spielfelds:
       1 2 3
     1
     2
@@ -54,42 +57,33 @@ def eingabe():
             return zug_koordinaten
 
 def update_spielfeld(gespielter_zug, aktiver_spieler):
+    """Setzt die Spielzüge in das Spielfeld"""
     global spielfeld
     x_kord, y_kord = gespielter_zug
     spielfeld[y_kord][x_kord] = aktiver_spieler
-    return -aktiver_spieler    
+    return -aktiver_spieler          
 
-def random_zug():
-    while True:
-        eingegebener_zug = randrange(1, 3), randrange(1, 3)
-        zug_erlaubt, zug_koordinaten = eingabe_pruefen(eingegebener_zug)
-        if zug_erlaubt:
-            return zug_koordinaten
-        
 def minimax_zug():
+    """Erstellt die erste Ebene des Spielbaums und ruft Minimax Funktion auf.
+    Wählt den besten Zug aus und sezt diesen auf das Spielfeld."""
     global spielfeld
     beste_bewertung = -1000
     bester_zug = None
-
     for i in range(3):
         for j in range(3):
             if spielfeld[i][j] == 0:
                 spielfeld[i][j] = 1
                 bewertung = minimax(spielfeld, 0, False)
                 spielfeld[i][j] = 0
-                #print(f"Bewertung für Zug ({i+1},{j+1}): {bewertung}")
                 if bewertung > beste_bewertung:
                     beste_bewertung = bewertung
                     bester_zug = (j, i)
-
     if bester_zug:
         x_kord, y_kord = bester_zug
         spielfeld[y_kord][x_kord] = 1
-        #print(f"Minimax wählt Zug ({y_kord+1},{x_kord+1})")
-
         
-
 def spiel_status():
+    """Druckt den Gewinner und das beendete Spiel in das Terminal"""
     ergebnis = evaluation(spielfeld)
     if ergebnis is not None:
         if ergebnis == 1:
@@ -103,25 +97,23 @@ def spiel_status():
     return False
 
 def game_loop():
-    #Zug ausführen
-    #Spielfeld updaten
-    #nächster Zug
+    """
+    Solange das Spiel nicht beendet ist, 
+    fürt es den Zug aus, 
+    aktualisiert das Spielfeld 
+    und ändert den aktiven Spieler.
+    """
     runde_ende = False
-    aktiver_spieler = -1
+    aktiver_spieler = -1 # Spieler X beginnt
     while runde_ende == False:
         spielfeld_anzeigen()
-        if aktiver_spieler == -1: #-1 = Mensch 
+        if aktiver_spieler == -1: # -1 = Spieler X 
             gespielter_zug = eingabe()
             aktiver_spieler = update_spielfeld(gespielter_zug, aktiver_spieler)
         else:
             gespielter_zug = minimax_zug()
-            aktiver_spieler = aktiver_spieler *-1
-
-            #für zwei Spieler(Menschen)
-            #gespielter_zug = eingabe()
-            #aktiver_spieler = update_spielfeld(gespielter_zug, aktiver_spieler)
-        
+            aktiver_spieler = aktiver_spieler *-1       
+    
         runde_ende = spiel_status()
-    #ausgang_anzeigen()
 
-game_loop()
+game_loop() # Startet das Spiel
